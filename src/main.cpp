5df7728e910897225cecb929a438b09074aff04e
TheNuRii <maciekglow0@gmail.com>
2026-03-28 23:31:16 +0100

    refactoring mess but if I want to comeback to it

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,83 @@
 #include <cstdlib>
 #include <cstdio>
 #include <unistd.h>
+#include <vector>
+#include <unordered_map>
 
-enum class Command {
-  EXIT,
-  ECHO,
-  TYPE,
-  UNKNOWN
+struct Command {
+  std::string name;
+  std::vector<std::string> args;
 };
+
+Command split_user_input(const std::string& input) {
+  Command cmd;
+
+  std::istringstream iss(input);
+  iss >> cmd.name;
+
+  for (std::string arg; iss; ) {
+    cmd.args.push_back(arg);
+  }
+
+  return cmd;
+}
+
+std::string find_path_to_command(const std::string& command) {
+  const char* path_to_env = std::getenv("PATH");
+  if (path_to_env == nullptr) {
+    return "";
+  }
+  std::string path_env(path_to_env);
+  std::stringstream ss_path(path_env);
+  std::string path;
+  
+  while (std::getline(ss_path, path, ':')) {
+    std::string full_path = path + "/" + command;
+    if (access(full_path.c_str(), X_OK) == 0) {
+        return full_path;
+    }
+  }
+
+  return "";
+}
+
+bool is_builtin(std::string& cmd) {
+  static const std::unordered_map<std::string, bool> builtins = {
+    {"exit", true},
+    {"echo", true},
+    {"type", true},
+  };
+
+  return builtins.find(cmd) != builtins.end();
+}
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
+  std::vector<std::string> builtins = {"exit", "echo", "type"};
   std::string user_input;
 
   while(std::getline(std::cin, user_input)){
     // prase user input by space and execute command
+    /*
+    std::vector<std::string> tokens;
     size_t pos = user_input.find(' ');
     std::string command = user_input.substr(0, pos);
     std::string command_args = (pos == std::string::npos) ? "" : user_input.substr(pos + 1); // just checking for empty string (no args)
-
+    */
+    std::vector<std::string> tokens = SplitUserInput(user_input);
+    if (tokens.empty()) {
+      std::cout << '\n' << "$ " ;
+      continue;
+    }
+    std::string command = tokens[0];
+  
     if (command == "exit") break;
-    else if (command == "type"){
-      if (command_args == "exit" || command_args == "echo" || command_args == "type") {
+
+    if (command == "type"){
+      if (command_arg == "exit" || command_args == "echo" || command_args == "type") {
         std::cout << command_args << " is a shell builtin\n" << "$ ";
         continue;
       }
