607148d547374d601ce18b904fbf190a8c532831
TheNuRii <maciekglow0@gmail.com>
2026-03-28 23:56:48 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <vector>
 #include <unordered_map>
 
@@ -12,7 +13,7 @@ struct Command {
   std::vector<std::string> args;
 };
 
-Command split_user_input(const std::string& input) {
+Command parse_user_input(const std::string& input) {
   Command cmd;
 
   std::istringstream iss(input);
@@ -54,72 +55,107 @@ bool is_builtin(std::string& cmd) {
   return builtins.find(cmd) != builtins.end();
 }
 
+bool execute_builtin(Command cmd) {
+
+  if (cmd.name == "exit") 
+    return false;
+  
+  else if (cmd.name == "echo") {
+    if (!cmd.args.empty()) {
+      std::cout << cmd.args[0] << '\n';
+    }
+  }
+
+  else if (cmd.name == "type") {
+    std::string arg = cmd.args[0];
+
+    if (is_builtin(arg)) {
+      std::cout << arg << " is a shell builtin" << '\n';
+    } else {
+      std::string executable_path = find_path_to_command(arg);
+
+      if (!executable_path.empty()) {
+      std::cout << arg << " is " << executable_path << '\n';
+      } else {
+        std::cout << arg << ": not found\n"; 
+      }
+    }
+  }
+
+  return true;
+}
+
+
+void execute_external_command(Command cmd) {
+  std::string executable_path = find_path_to_command(cmd.name);
+
+  if (executable_path.empty()) {
+    std::cout << cmd.name << ": command not found\n";
+    return;
+  }
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    // Child process
+    std::string executable_path = find_path_to_command(cmd.name);
+
+    std::vector<std::string> arg_sep_space;
+    arg_sep_space.push_back(cmd.name);
+
+    std::string arg = cmd.args[0];
+    size_t j{0};
+
+    for (size_t i{0}; i < arg.size(); ++i) {
+      if (arg[i] == ' ') {
+        if (i > j) {
+          arg_sep_space.push_back(arg.substr(j, i - j));
+        }
+
+        j = i + 1;
+      }
+    }
+
+    if (j < arg.size()) 
+      arg_sep_space.push_back(arg.substr(j));
+    
+    std::vector<char *> argp;
+
+    for (auto &s: arg_sep_space) {
+      argp.push_back(const_cast<char *>(s.c_str()));
+    }
+
+    argp.push_back(nullptr); // it expect nullptr to stop
+
+    execv(executable_path.c_str(), argp.data());
+
+  } else if (pid > 0) {
+    // Parent process
+    int status;
+    waitpid(pid, &status, 0);
+
+  } else {
+    std::cout << "Error: Failed to fork\n"; 
+  }
+}
 int main() {
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
-  std::vector<std::string> builtins = {"exit", "echo", "type"};
   std::string user_input;
+  while (std::getline(std::cin, user_input)) {
+    Command cmd = parse_user_input(user_input);
 
-  while(std::getline(std::cin, user_input)){
-    // prase user input by space and execute command
-    /*
-    std::vector<std::string> tokens;
-    size_t pos = user_input.find(' ');
-    std::string command = user_input.substr(0, pos);
-    std::string command_args = (pos == std::string::npos) ? "" : user_input.substr(pos + 1); // just checking for empty string (no args)
-    */
-    std::vector<std::string> tokens = SplitUserInput(user_input);
-    if (tokens.empty()) {
-      std::cout << '\n' << "$ " ;
-      continue;
+    if (cmd.name.empty()) continue;
+
+    if (is_builtin(cmd.name)) {
+      if (!execute_builtin(cmd))
+        break;
+    } else {
+      execute_external_command(cmd);
     }
-    std::string command = tokens[0];
-  
-    if (command == "exit") break;
-
-    if (command == "type"){
-      if (command_arg == "exit" || command_args == "echo" || command_args == "type") {
-        std::cout << command_args << " is a shell builtin\n" << "$ ";
-        continue;
-      }
-
-      const char* path_env_cstr = std::getenv("PATH");
-      if (path_env_cstr == nullptr) {
-        std::cerr << command_args << ": not found\n" << "$ ";        
-        continue;
-      }
-
-      std::string path_env = std::getenv("PATH");
-      std::stringstream ss_path(path_env);
-      std::string path;
-      //printf("PATH: %s\n", path_env.c_str());
-      //printf("Command args: %s\n", command_args.c_str());
-      bool found = false;
-
-      while (std::getline(ss_path, path, ':')) {
-        std::string full_path = path + "/" + command_args;
-        if (access(full_path.c_str(), X_OK) == 0) {
-            std::cout << command_args << " is " << full_path << "\n" << "$ ";
-            found = true;
-            break;
-        }
-      }
-      
-      if (!found) {
-        std::cerr << command_args << ": not found\n" << "$ ";
-      }
-      continue;
-    }
-
-    if (command == "echo") {
-      std::cout << command_args << '\n';
-      std::cout << "$ ";
-      continue;
-    }
-    std::cerr << command << ": command not found\n" << "$ ";
   }
-  
   return 0;
 }
