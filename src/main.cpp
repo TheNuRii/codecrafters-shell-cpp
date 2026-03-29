ddd6b8cf3ae78674a4522c3d1b82ecda2a974193
TheNuRii <maciekglow0@gmail.com>
2026-03-29 16:02:42 +0200

    The cd builtin: Home directory

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,16 @@ bool is_builtin(std::string& cmd) {
   return builtins.find(cmd) != builtins.end();
 }
 
+std::string find_home_dir() {
+  const char* path_to_home_dir = std::getenv("HOME");
+
+  if (path_to_home_dir == nullptr) {
+    std::cout << "Your operating system is broken :)";
+  }
+
+  return std::string(path_to_home_dir);
+}
+
 bool execute_builtin(Command cmd) {
 
   if (cmd.name == "exit") 
@@ -96,12 +106,17 @@ bool execute_builtin(Command cmd) {
   else if (cmd.name == "cd") {
     if (cmd.args.empty()) {
       std::cout << '\n';
-    } else {
+    }else {
       std::string new_path = cmd.args[0];
+      
+      if (new_path == "~") {
+        new_path = find_home_dir();
+      } 
+
       try {
         std::filesystem::current_path(new_path);
       } catch (const std::filesystem::filesystem_error& e) {
-      std::cout << "cd: " << new_path << ": No such file or directory\n";
+        std::cout << "cd: " << new_path << ": No such file or directory\n";
       }
     }
   }
@@ -131,7 +146,6 @@ void execute_external_command(const Command &cmd) {
 
         execv(executable_path.c_str(), argv.data());
 
-        // If execv returns, there was an error
         perror("execv");
         _exit(EXIT_FAILURE);
     } 
